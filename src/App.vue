20e18b09147fd6846d5eb256315074a84b9eb6de
Matheus <matheuspsilva29@gmail.com>
2021-10-15 12:00:26 -0300

    iniciando template

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>{{ titulo }}</h1>
+    <img :src="foto.url" :alt="foto.titulo">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      titulo:'Alurapic',
+      foto: {
+        url:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+        titulo: 'cachorro'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

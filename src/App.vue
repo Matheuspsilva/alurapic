2e5e4d5c3e36fe141fc578c26a991e477a7311e5
Matheus <matheuspsilva29@gmail.com>
2021-10-15 13:19:26 -0300

    Adiçao de dados via array

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
+
     <h1>{{ titulo }}</h1>
-    <img :src="foto.url" :alt="foto.titulo">
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
 
   </div>
 </template>
@@ -11,10 +16,17 @@ export default {
   data(){
     return{
       titulo:'Alurapic',
-      foto: {
-        url:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-        titulo: 'cachorro'
-      }
+      fotos: [
+        {
+          url:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+          titulo: 'cachorro'
+        },
+        {
+          url:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+          titulo: 'labrador'
+        }
+      ]
+
     }
   }
 }
